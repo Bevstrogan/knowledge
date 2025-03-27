1a149ef936d19bdca5617de9112e7be16aa40118
Bevstrogan <bevstrogan@gmail.com>
2025-03-28 00:39:09 +0300

    static + views + templates

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from dogs.apps import DogsConfig
+from dogs.views import dogs_list, dogs_detail
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-
+    path('', dogs_list, name='dogs_list'),
+    path('dogs/<int:pk>/', dogs_detail, name='dogs_detail')
 ]
